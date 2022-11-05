104d08df71f92c997b5d58e67d1cef7068f4da47
joe <risnerjoseph@gmail.com>
2022-11-05 00:19:51 -0600

    Update header and layout for responsiveness

--- specs/navigation.spec.ts
+++ specs/navigation.spec.ts
@@ -1,25 +1,40 @@
 import { test, expect } from '@playwright/test';
-import { capitalize } from './utils/string.js';
 
-test.describe('Navigation Links', () => {
+test.describe('Navigation - Large Viewport', () => {
   test('navigates to each page successfully', async ({ page }) => {
     await page.goto('/');
 
-    const expectedLinks = ['home', 'blog', 'about', 'resume'];
+    const expectedLinks = ['Blog', 'Projects', 'About'];
     const allNavLinks = page.getByTestId('navLink');
 
+    await expect(page.getByTestId('mobileNavToggle')).not.toBeVisible();
     await expect(allNavLinks).toHaveText(expectedLinks, { ignoreCase: false });
 
     for (const link of expectedLinks) {
       await page.locator('[data-testid=navLink]', { hasText: link }).click();
 
-      if (link === 'home') {
-        await expect(page).toHaveTitle('Joseph Risner');
-        await expect(page).toHaveURL('/');
-      } else {
-        await expect(page).toHaveTitle(`${capitalize(link)} | Joseph Risner`);
-        await expect(page).toHaveURL(`/${link}`);
-      }
+      await expect(page).toHaveTitle(`${link} | Joseph Risner`);
+      await expect(page).toHaveURL(`/${link.toLowerCase()}`);
+    }
+  });
+});
+
+test.describe('Navigation - Mobile Viewport', () => {
+  test('displays navigation overlay with nav links', async ({ page }) => {
+    await page.setViewportSize({ width: 767, height: 940 });
+    await page.goto('/');
+
+    const expectedLinks = ['Blog', 'Projects', 'About'];
+    const navToggle = page.getByTestId('mobileNavToggle');
+
+    await expect(navToggle).toBeVisible();
+    await navToggle.click();
+
+    for (const link of expectedLinks) {
+      const el = page.locator('[data-testid=navLink]', { hasText: link });
+
+      await expect(el).toBeVisible();
+      await expect(el).toHaveAttribute('href', `/${link.toLowerCase()}`);
     }
   });
 });
