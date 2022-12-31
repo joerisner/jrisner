3ad7f7e1ec1f0d56b566976ddc096617fe7241a4
joe <risnerjoseph@gmail.com>
2022-12-30 20:15:49 -0700

    Bump Node version and update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-// See https://playwright.dev/docs/test-configuration.
 const config: PlaywrightTestConfig = {
   expect: {
-    timeout: 6000, // Maximum time expect() should wait for the condition to be met
+    timeout: 6000,
   },
-  fullyParallel: true, // Run test blocks within files in parallel
-  forbidOnly: !!process.env.CI, // Fail the build on CI for .only left in source code
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
   outputDir: './specs/artifacts',
   projects: [
     {
@@ -26,20 +25,23 @@ const config: PlaywrightTestConfig = {
   reporter: [['dot'], ['html', { open: 'never', outputFolder: './specs/report' }]],
   retries: process.env.CI ? 2 : 0, // Retry on CI only
   testDir: './specs',
-  timeout: 30 * 1000, // Maximum time one test can run for
+  timeout: 30 * 1000, // Maximum time one test can run
   // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions
   use: {
-    actionTimeout: 0, // Maximum time each action such as `click()` can take. Defaults to 0 (no limit)
-    baseURL: 'http://localhost:4000',
-    screenshot: 'only-on-failure',
-    trace: 'on-first-retry', // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
+    actionTimeout: 0, // 0 = no limit
+    baseURL: 'http://127.0.0.1:4000', // NOTE: Can be changed back to localhost after v1.30 https://github.com/microsoft/playwright/issues/18790
+    screenshot: {
+      mode: 'only-on-failure',
+      fullPage: true,
+    },
+    trace: 'on-first-retry',
   },
   // Start web server before running tests
   webServer: {
     command: 'yarn preview',
-    reuseExistingServer: !process.env.CI, // Skip reusing an existing server on CI
-    timeout: 120 * 1000,
-    url: 'http://localhost:4000', // Waits for 2xx, 3xx, or 4xx response before running tests
+    reuseExistingServer: !process.env.CI,
+    timeout: 30 * 1000,
+    url: 'http://127.0.0.1:4000', // Waits for 2xx, 3xx, or 4xx response before running tests. See note on `baseURL` above, as well
   },
   workers: process.env.CI ? 1 : undefined, // Opt out of parallel tests on CI
 };
