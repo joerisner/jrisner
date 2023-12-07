36ae34b6b0b2d5035524d91731d9ed6ec585b9cd
joe <risnerjoseph@gmail.com>
2023-12-06 21:35:34 -0700

    Remove blog

--- specs/home.spec.ts
+++ specs/home.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 test.describe('Home Page', () => {
   test('has expected behavior for links on page', async ({ page }) => {
     const githubLink = page.getByTestId('githubLink');
-    const blogLink = page.getByTestId('cta');
+    const ctaButton = page.getByTestId('cta');
 
     await page.goto('/');
     await expect(githubLink).toBeVisible();
     await expect(githubLink).toHaveAttribute('href', 'https://github.com/joerisner');
-    await expect(blogLink).toHaveText('Check out the cheat sheets');
-    await expect(blogLink).toHaveAttribute('href', '/cheat-sheets');
+    await expect(ctaButton).toHaveText('Check out the cheat sheets');
+    await expect(ctaButton).toHaveAttribute('href', '/cheat-sheets');
   });
 });
