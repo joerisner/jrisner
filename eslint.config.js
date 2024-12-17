201a81fa253295e4c7c66e4d379ffbbfe19ff0b9
joe <joe.risner@proton.me>
2024-12-16 17:35:46 -0700

    Add /blog back to project

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import js from '@eslint/js';
-import globals from 'globals';
 
 export default [
   // Use ESLint recommended
   js.configs.recommended,
   {
-    languageOptions: { globals: { ...globals.node } },
     rules: {
       'max-nested-callbacks': ['error', 3],
       'no-return-await': 'error',
